844154a891777f51501840d7faf58706ecde5421
836972159@qq.com <836972159@qq.com>
2022-08-09 16:55:36 +0800

    config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,15 @@
 // .vuepress/config.js
 module.exports = {
+  title:'MOOC-UI',
+  nav:[{
+    text:'Home',link:'/'
+  },{
+    text:'Github',link:'https://github.com/wangcan567/moocUI'
+  },{
+    text:'Npm',link:'https://www.npmjs.com/package/mooc-cancan-ui'
+  }],
   /* 配置base */
-  base: '/moocUI',
+  base: '/moocUI/',
   themeConfig: {
     sidebar: [
       '/',
