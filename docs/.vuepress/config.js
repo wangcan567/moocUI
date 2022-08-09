a36c2cd4ed9b5df1684921df2901d34a3452fda5
836972159@qq.com <836972159@qq.com>
2022-08-09 17:10:04 +0800

    文档修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,19 @@
 // .vuepress/config.js
 module.exports = {
-  title:'MOOC-UI',
-  nav:[{
-    text:'Home',link:'/'
-  },{
-    text:'Github',link:'https://github.com/wangcan567/moocUI'
-  },{
-    text:'Npm',link:'https://www.npmjs.com/package/mooc-cancan-ui'
-  }],
+  title: 'MOOC-UI',
   /* 配置base */
   base: '/moocUI/',
   themeConfig: {
+    nav: [{
+      text: 'Home',
+      link: '/'
+    }, {
+      text: 'Github',
+      link: 'https://github.com/wangcan567/moocUI'
+    }, {
+      text: 'Npm',
+      link: 'https://www.npmjs.com/package/mooc-cancan-ui'
+    }],
     sidebar: [
       '/',
       'componentDocs/card'
